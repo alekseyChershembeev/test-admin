1e9a00a5ce3e87bff90ebbe07e72b38008343386
alekseyChershembeev <aleksey.chershembeev@gmail.com>
2020-04-09 14:03:52 +0300

    add new fiches need add nw provider for example: https://github.com/vishpat/ra-data-springboot-rest/blob/master/src/index.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import jsonServerProvider from 'ra-data-json-server';
 import { Admin, Resource} from 'react-admin';
 import {UserList} from "./Users";
 import { PostList, PostEdit, PostCreate } from './Posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
+import Dashboard from './Dashboard'
+import authProvider from './authProvider';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-        <Admin dataProvider={dataProvider}>
-            <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} />
-            <Resource name="users" list={UserList} />
-          </Admin>
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+            <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+            <Resource name="users" list={UserList}  icon={UserIcon}/>
+    </Admin>
     );
 export default App;
--- src/Posts.js
+++ src/Posts.js
@@ -10,34 +10,62 @@ import {
     ReferenceInput,
     SelectInput,
     TextInput,
-    Create
+    Create,
+    Filter,
+    SimpleList
 } from 'react-admin';
 
-export const PostList = props => (
-    <List {...props}>
-        {/*<Datagrid rowClick="edit">*/}
+import { useMediaQuery } from '@material-ui/core';
 
-        <Datagrid>
-            <TextField source="id"/>
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name"/>
-            </ReferenceField>
-            <TextField source="id"/>
-            <TextField source="title"/>
-            <TextField source="body"/>
-            <EditButton/>
-        </Datagrid>
-    </List>
-);
+
+
+export const PostList = (props) => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down('sm'));
+    return (
+        <List filters={<PostFilter />} {...props}>
+            {isSmall ? (
+                <SimpleList
+                    primaryText={record => record.title}
+                    secondaryText={record => `${record.views} views`}
+                    tertiaryText={record => new Date(record.published_at).toLocaleDateString()}
+                />
+            ) : (
+                <Datagrid>
+                    <TextField source="id" />
+                    <ReferenceField label="User" source="userId" reference="users">
+                        <TextField source="name" />
+                    </ReferenceField>
+                    <TextField source="title" />
+                    <TextField source="body" />
+                    <EditButton />
+                </Datagrid>
+            )}
+        </List>
+    );
+};
+
+// export const PostList = props => (
+//     <List filters={<PostFilter />} {...props}>
+//         <Datagrid>
+//             <TextField source="id"/>
+//             <ReferenceField source="userId" reference="users">
+//                 <TextField source="name"/>
+//             </ReferenceField>
+//             <TextField source="id"/>
+//             <TextField source="title"/>
+//             <TextField source="body"/>
+//             <EditButton/>
+//         </Datagrid>
+//     </List>
+// );
 
 export const PostEdit = props => (
        <Edit title={<PostTitle />} {...props}>
         <SimpleForm>
             <TextInput disabled source="id"/>
             <ReferenceInput source="userId" reference="users">
-                <SelectInput optionText="id"/>
+                <SelectInput optionText="name"/>
             </ReferenceInput>
-            <SelectInput optionText="name"/>
             <TextInput source="title"/>
             <TextInput multiline source="body"/>
         </SimpleForm>
@@ -59,6 +87,20 @@ export const PostCreate = props => (
 const PostTitle = ({ record }) => {
         return <span>Post {record ? `"${record.title}"` : ''}</span>;
     };
+
+
+const PostFilter = (props) => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput label="User" source="userId" reference="users" allowEmpty>
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+
+
+
 
 
 
